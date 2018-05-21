9c766caa58d3cf9271b12e3ad630fd8d06755b45
kman12681 <keith.rigling@gmail.com>
2018-05-21 17:14:27 -0400

    first commit

---
+++ Lab_21/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_21.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register(
+            string firstName="John", 
+            string lastName="Smith", 
+            string email="John@Smith.com", 
+            int number= 0000000000, 
+            string password="password")
+        {
+            ViewBag.FirstName = firstName;
+            ViewBag.LastName = lastName;
+            ViewBag.Email = email;
+            ViewBag.Number = number;
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
