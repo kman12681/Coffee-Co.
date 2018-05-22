bed6442af00238fd173babb664cfb58dbc585628
kman12681 <keith.rigling@gmail.com>
2018-05-22 15:27:41 -0400

    added two fields

--- Lab_21/Controllers/UserController.cs
+++ Lab_21/Controllers/UserController.cs
@@ -15,17 +15,21 @@ namespace Lab_21.Controllers
         }
 
         public ActionResult Register(
-            string firstName="John", 
-            string lastName="Smith", 
-            string email="John@Smith.com", 
-            int number= 0000000000, 
-            string password="password")
+            string firstName = "John",
+            string lastName = "Smith",
+            string email = "John@Smith.com",
+            int number = 0000000000,
+            string password = "password",
+            string birthday = "",
+            string strength = "medium")
         {
             ViewBag.FirstName = firstName;
             ViewBag.LastName = lastName;
             ViewBag.Email = email;
             ViewBag.Number = number;
             ViewBag.Password = password;
+            ViewBag.Birthday = birthday;
+            ViewBag.Strength = strength;
             return View();
         }
     }
